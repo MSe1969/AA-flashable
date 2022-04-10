141f586beae1d29eb3c52064fbdfe3240064a42c
MSe <mse1969@posteo.de>
2022-04-10 17:34:17 +0200

    Adaptations for /system_root and certain TWRP / device situations
    Certain devices do not set the property 'ro.build.system_root_image', but
    in TWRP, the /system_root partition is used and mounted, but /system is used
    for TWRP itself. Hence we need to work around this (ugly hack).

--- install/unmount-system.sh
+++ install/unmount-system.sh
@@ -1,8 +1,10 @@
 #!/sbin/sh
 
 # Modern devices use /system as root ("/")
+# TWRP 3.6.x sometimes also uses /system_root,
+# even if property not present
 system_as_root=`getprop ro.build.system_root_image`
-if [ "$system_as_root" == "true" ]; then
+if [ "$system_as_root" == "true" ] || [ -d "/system_root" ]; then
   if umount /system_root; then
     exit 0
   fi
